7327f882ec9e86ea6016d3a9400f83c34e5e986d
Allam0r <florian.wiart05@gmail.com>
2022-11-03 17:33:46 +0100

    add script (index)

---
+++ front/js/script.js
@@ -0,0 +1,33 @@
+// **********************************************************************
+
+// Récupération des info de l'API
+fetch("http://localhost:3000/api/products")
+  // Une fois les infos obtenus donnent le résultat en Json
+  .then((response) => response.json())
+  //   Donne moi les infos sur la console sous forme de tableau
+  .then((data) => {
+    console.table(data);
+    // Fonction d'appel d'affichage des produits
+    listKanap(data);
+  });
+
+// **********************************************************************
+
+// Fonction d'affichage des items de l'API sur la page Index
+function listKanap(index) {
+  // On déclare la zone 'articleCard' dans l'HTML
+  let articleCards = document.querySelector("#items");
+  // Création de la boucle article dans Index
+  for (let article of index) {
+    // Mise en forme et récupération des infos: id, imageUrl, altTxt, name, desc pour chaques articles de la data
+    articleCards.innerHTML += `
+        <a href="./product.html?_id${article._id}">
+        <article>
+        <img src='${article.imageUrl}' alt='${article.altTxt}'>
+        <h3 class='productName'>${article.name}</h3>
+        <p class='productDescription'>${article.description}
+        </article>
+        </a>
+        `;
+  }
+}
